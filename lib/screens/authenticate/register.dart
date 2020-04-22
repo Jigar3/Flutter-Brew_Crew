ab1a5b0d4138495b7e8790bdca9a8e6b6e1d4cc6
Jigar <chavada.jigar.3@gmail.com>
2020-04-22 16:02:02 +0530

    added code

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,6 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
+import 'package:brew_crew/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -13,13 +15,16 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
+  final _formkey = GlobalKey<FormState>();
   
   String email = "";
   String password = "";
+  String error = "";
+  bool loading = false;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return loading ? Loading() : Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
@@ -38,16 +43,21 @@ class _RegisterState extends State<Register> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formkey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Email'),
+                validator: (val) => val.isEmpty ? 'Enter a valid email ID' : null,
                 onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                decoration: textInputDecoration.copyWith(hintText: 'Password'),
+                validator: (val) => val.length < 6 ? 'Enter a password 6+ characters long' : null,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
@@ -61,9 +71,23 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if(_formkey.currentState.validate()) {
+                    setState(() => loading = true);
+                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() {
+                        error = "Please Supply a valid email";
+                        loading = false;
+                        }
+                      );
+                    }
+                  }
                 },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 14.0)
               )
             ],
           )
